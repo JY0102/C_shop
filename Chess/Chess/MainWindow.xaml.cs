7db0e12fbd939aeba9bbff693123efa281fd1ef9
JY0102 <58729002+JY0102@users.noreply.github.com>
2025-04-29 09:18:15 +0900

    Add files via upload

---
+++ Chess/Chess/MainWindow.xaml.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chess
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Button[,] btns = new Button[8, 8];
+
+        #region 겜 시작
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+        public void Start()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Button btn = new Button();
+
+                    btn.Background = this.Background;
+                    btn.Click += Btn_Click;
+
+                    btns[x, y] = btn;
+
+                    Grid.SetColumn(btn, x);
+                    Grid.SetRow(btn, y);
+
+                    chess.Children.Add(btn);
+
+                    SetBorad(btns, x, y);
+                    CreateObject(x, y);
+                }
+            }
+            SetDanger();
+            SetPieces();
+        }
+        private void CreateObject(int x, int y)
+        {
+            if (y == 7)
+            {
+                btns[x, y].Foreground = Brushes.Blue;
+            }
+            else
+            {
+                btns[x, y].Foreground = Brushes.Black;
+            }
+
+            if ((x == 0 || x == 7) && (y == 7 || y == 0))
+            {
+                btns[x, y].Content = "룩";
+            }
+            else if ((x == 1 || x == 6) && (y == 7 || y == 0))
+            {
+                btns[x, y].Content = "말";
+            }
+            else if ((x == 2 || x == 5) && (y == 7 || y == 0))
+            {
+                btns[x, y].Content = "비숍";
+            }
+            else if (x == 3 && (y == 7 || y == 0))
+            {
+                btns[x, y].Content = "퀸";
+            }
+            else if (x == 4 && (y == 7 || y == 0))
+            {
+                btns[x, y].Content = "킹";
+            }
+            else if (y == 1)
+            {
+                btns[x, y].Content = "폰";
+                btns[x, y].Foreground = Brushes.Black;
+            }
+            else if (y == 6)
+            {
+                btns[x, y].Content = "폰";
+                btns[x, y].Foreground = Brushes.Blue;
+            }
+        }
+        private void SetDanger()
+        {
+            for (int y = 5; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Black_Danger[x, y] = true;
+                }
+            }
+
+            for(int y= 0; y<3; y++)
+            {
+                for(int x=0; x<8; x++)
+                {
+                    White_Danger[x, y] = true;
+                }
+            }
+        }
+        private void SetBorad(Button[,] btns , int x, int y)
+        {
+            if ((x + y) % 2 == 0)
+                btns[x,y].Background = Brushes.Beige;
+            else
+                btns[x,y].Background = Brushes.DarkSeaGreen;
+        }
+        private void SetPieces()
+        {
+            for(int y =0; y< 8; y++)
+            {
+                for(int x=0; x< 8; x++)
+                {
+                    if(btns[x,y].Content != null)
+                    {
+                        Enables[x, y] = true;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        private bool[,] Enables = new bool[8, 8];
+
+        // 블랙 , 화이트 왕이 가면 안되는 지역
+        private bool[,] Black_Danger = new bool[8, 8];
+        private bool[,] White_Danger = new bool[8, 8];
+
+        // 왕 위치
+        private int[] Black_King = new int[] { 4, 0 };
+        private int[] White_King = new int[] { 4, 7 };
+
+        private int Before_X = -1;
+        private int Before_Y = -1;
+
+        private Brush Before_Color = null;
+
+        private List<int[]> Location = null;
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                e.Handled = true;
+            }
+
+            Button  btn = (Button)sender;
+
+
+            int x = Grid.GetColumn(btn);
+            int y = Grid.GetRow(btn);
+
+            //첫 클릭
+            if(Before_X == -1 )
+            {
+                if (btn.Content == null)
+                    return;
+
+                Before_X = x;
+                Before_Y = y;
+                Before_Color = btn.Foreground;
+
+                Location = Piece.MovePiece(btn, x, y , Enables);
+
+                foreach ( var item in Location)
+                {
+                    btns[item[0], item[1]].Background = Brushes.LightGreen;
+                }
+                btn.Background = Brushes.LightBlue;
+            }
+            // 나중 클릭
+            else
+            {
+                if (Before_X == x && Before_Y == y)
+                {
+                    Reset(btn);
+                }
+
+                if (Location.FindIndex((temp) => temp[0] == Grid.GetColumn(btn) && temp[1] == Grid.GetRow(btn)) == -1)
+                    return;
+
+
+                #region 움직임 -> 이름 변경 + 이름 색 맞춤
+                btn.Content = btns[Before_X, Before_Y].Content;
+                btns[Before_X, Before_Y].Content = null;
+
+                btn.Foreground = Before_Color;
+
+                Enables[Before_X, Before_Y] = false;
+                Enables[x,y] = true;
+                #endregion
+
+                #region 체크 확인
+                if (Check_Checking(btn))
+                    MessageBox.Show("체크");
+                #endregion
+
+                Reset(btn);
+            }
+        }
+        private bool Check_Checking(Button btn)
+        {
+            var list = Piece.MovePiece(btn, Grid.GetColumn(btn), Grid.GetRow(btn) , Enables);
+
+            foreach (var item in list)
+            {
+                if (btn.Foreground == Brushes.Black)
+                {
+                    if (White_King[0] == item[0] && White_King[1] == item[1])
+                        return true;
+                }
+                else
+                {
+                    if (Black_King[0] == item[0] && Black_King[1] == item[1])
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        private void Reset(Button btn)
+        {
+            if ((string)btn.Content == "킹")
+            {
+                if (btn.Foreground == Brushes.Black)
+                    Black_King = new int[] { Grid.GetColumn(btn), Grid.GetRow(btn) };
+                else
+                    White_King = new int[] { Grid.GetColumn(btn), Grid.GetRow(btn) };
+            }
+
+            SetBorad(btns, Before_X , Before_Y);
+
+            foreach (var item in Location)
+            {
+                SetBorad(btns, item[0], item[1]);
+            }
+            Location.Clear();
+
+            Before_X = -1;
+            Before_Y = -1;
+            Before_Color = null;
+        }
+    }
+}
---
+++ Chess/Chess/Piece.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Chess
+{
+    internal class Piece
+    {
+        private static bool[,] Enables = new bool[8,8];
+        public static List<int[]> MovePiece(Button btn, int x, int y, bool[,] e)
+        {
+            Enables = e;
+
+            switch(btn.Content.ToString())
+            {
+                case "폰":   return MovePawn(btn.Foreground, x, y);
+                case "룩":   return MoveRook(x, y);
+                case "말":   return MoveKnight(x, y);
+                case "비숍": return MoveBishop(x, y);
+                case "퀸":   return MoveQueen(x, y);
+                case "킹":   return MoveKing(x, y);
+            }
+            return null;
+        }
+
+        public static List<int[]> MovePawn(Brush color , int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            if(color == Brushes.Black)
+            {
+                if (y == 1)
+                {
+                    if(!Enables[x, y + 1])
+                        list.Add(new int[] { x , y + 1 });
+                    if(!Enables[x, y + 2])
+                        list.Add(new int[] { x , y + 2 });
+                }
+                else if( y == 7)
+                {
+                    // 할꺼
+                    // 폰 기물변환
+                }
+                else
+                {
+                    if(!Enables[x,y +1])
+                        list.Add(new int[] { x, y + 1 });
+
+                    if (Enables[Math.Max( 0 , x - 1 ) , y+1])
+                    {
+                        list.Add(new int[] { x -1 , y+1 });
+                    }
+                    if (Enables[x + 1, y + 1])
+                    {
+                        list.Add(new int[] {x +1 , y + 1 });
+                    }
+
+                }
+            }
+            else
+            {
+                if (y == 6)
+                {
+                    if (!Enables[Math.Max(0, x - 1), y - 1])
+                        list.Add(new int[] { x, y - 1 });
+                    if (!Enables[x, y - 2])
+                        list.Add(new int[] { x, y - 2 });
+                }
+                else if( y == 0)
+                {
+                    // 할꺼
+                    // 폰 기물변환
+                }
+                else
+                {
+                    if (!Enables[x, y - 1])
+                        list.Add(new int[] { x, y - 1 });
+
+                    if (Enables[Math.Max(0, x - 1), y - 1])
+                    {
+                        list.Add(new int[] { x - 1, y - 1 });
+                    }
+                    if (Enables[x + 1, y - 1])
+                    {
+                        list.Add(new int[] { x + 1, y - 1 });
+                    }
+                }
+            }
+
+            return list;
+        }
+        public static List<int[]> MoveRook(int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            // 상
+            for(int i= y + 1; i<8; i++)
+            {
+                if (Enables[x, i])
+                    break;
+
+                list.Add(new int[] {x,i});
+            }
+            // 우
+            for(int i= x + 1; i<8; i++)
+            {
+                if (Enables[i, y])
+                    break;
+
+                list.Add(new int[] { i , y });
+            }
+            // 하
+            for(int i = y - 1; i >= 0; i--)
+            {
+                if (Enables[x, i])
+                    break;
+
+                list.Add(new int[] { x , i });
+            }
+            // 좌
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (Enables[i, y])
+                    break;
+
+                list.Add(new int[] { i , y });
+            }
+
+            return list;
+        }
+        public static List<int[]> MoveKnight(int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            int[] dx = { -2, -2, 2, 2, -1, 1, -1, 1 };
+            int[] dy = { 1, -1, 1, -1, -2, -2, 2, 2 };
+
+            // 반복문으로 각 방향을 순회
+            for (int i = 0; i < dx.Length; i++)
+            {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if( newX  < 0 || newX > 7 )
+                     continue;
+                if (newY < 0 || newY > 7)
+                    continue;
+
+                list.Add(new int[] { newX, newY });
+            }
+
+            return list;
+        }
+        public static List<int[]> MoveBishop(int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            // 우 하
+            for(int i = 1; i < 8 - x; i++)
+            {
+                if (y + i < 8)
+                {
+                    if (Enables[x + i,  y + i])
+                        break;
+                    list.Add(new int[] { x + i , y + i});
+                }
+            }
+            // 우 상
+            for(int i = 1; i < 8 - x; i++)
+            {
+                if (y - i >= 0)
+                {
+                    if (Enables[x + i, y - i])
+                        break;
+                    list.Add(new int[] { x + i, y - i });
+                }
+            }
+            // 좌 하
+            for (int i = 1; i <= x; i++)
+            {
+                if (y + i < 8)
+                {
+                    if (Enables[x - i , y + i])
+                        break;
+                    list.Add(new int[] { x - i, y + i });
+                }
+            }
+            // 좌 상
+            for (int i = 1; i <= x; i++)
+            {
+                if (y - i >= 0 )
+                {
+                    if (Enables[x - i, y - i])
+                        break;
+                    list.Add(new int[] { x - i, y - i });
+                }
+            }
+
+
+
+            return list;
+        }
+        public static List<int[]> MoveQueen(int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            var temp1 = MoveRook(x,y);
+            var temp2 = MoveBishop(x,y);
+
+            list.AddRange(temp1);
+            list.AddRange(temp2);
+
+            return list;
+        }
+        public static List<int[]> MoveKing(int x, int y)
+        {
+            List<int[]> list = new List<int[]>();
+
+            int[] move_x = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] move_y = { -1, 0, 1, -1, 1, 1, 0, -1 };
+
+            for(int i=0; i<8; i++)
+            {
+                if (x + move_x[i] < 0 || x + move_x[i] > 7)
+                    continue;
+                if (y + move_y[i] < 0 || y + move_y[i] > 7)
+                    continue;
+                if (Enables[x + move_x[i], y + move_y[i]])
+                    continue;
+
+                list.Add(new int[] { x + move_x[i], y + move_y[i] });
+            }
+            return list;
+        }
+    }
+}
